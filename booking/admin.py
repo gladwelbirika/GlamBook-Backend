f7ea63d16eba95a6b74051f52a7374e17641433c
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-07-21 12:54:33 +0300

    add booking status and admin management

--- booking/admin.py
+++ booking/admin.py
@@ -4,6 +4,15 @@ from .models import Category, Service, Booking, Payment, Stylist
 admin.site.register(Category)
 admin.site.register(Service)
 admin.site.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'customer_name',
+        'stylist',
+        'service',
+        'date',
+        'time',
+        'status',
+    )
 admin.site.register(Payment)  
 admin.site.register(Stylist)  
 # Register your models here.
