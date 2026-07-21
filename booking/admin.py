55dd0eb6f136ba136053b00d36bf188ecdabb45b
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-07-21 10:27:31 +0300

    register models in Django and set up administrator access

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Service, Booking, Payment
 
+admin.site.register(Category)
+admin.site.register(Service)
+admin.site.register(Booking)
+admin.site.register(Payment)    
 # Register your models here.
