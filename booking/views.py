84edd5a6008107036fcb1a8de1d89e9d2078eae9
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-07-21 11:20:13 +0300

    create API viewsets for booking models

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Service, Stylist, Booking, Payment
+from .serializers import CategorySerializer, ServiceSerializer, StylistSerializer, BookingSerializer, PaymentSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+class StylistViewSet(viewsets.ModelViewSet):
+    queryset = Stylist.objects.all()
+    serializer_class = StylistSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
