10ca5c4c4ae736b5a7f6341d563ac301a67a81dd
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-07-22 14:22:57 +0300

    add jwt authentication and api permissions

--- booking/views.py
+++ booking/views.py
@@ -1,23 +1,38 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Category, Service, Stylist, Booking, Payment
-from .serializers import CategorySerializer, ServiceSerializer, StylistSerializer, BookingSerializer, PaymentSerializer
+from .serializers import (
+    CategorySerializer,
+    ServiceSerializer,
+    StylistSerializer,
+    BookingSerializer,
+    PaymentSerializer,
+)
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
 class StylistViewSet(viewsets.ModelViewSet):
     queryset = Stylist.objects.all()
     serializer_class = StylistSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
+
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAuthenticated]
