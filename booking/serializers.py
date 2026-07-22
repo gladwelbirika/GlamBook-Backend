b95d57bed167c3756445c7d6eda25ee6deee6d0f
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-07-22 14:52:47 +0300

    add booking validation rules

--- booking/serializers.py
+++ booking/serializers.py
@@ -25,6 +25,39 @@ class BookingSerializer(serializers.ModelSerializer):
         model = Booking
         fields = "__all__"
 
+    def validate_date(self, value):
+        from django.utils import timezone
+
+        if value < timezone.now().date():
+            raise serializers.ValidationError(
+                "Booking date cannot be in the past."
+            )
+
+        return value
+
+    def validate(self, data):
+        stylist = data.get("stylist")
+        date = data.get("date")
+        time = data.get("time")
+
+        existing_booking = Booking.objects.filter(
+            stylist=stylist,
+            date=date,
+            time=time
+        )
+
+        if self.instance:
+            existing_booking = existing_booking.exclude(
+                id=self.instance.id
+            )
+
+        if existing_booking.exists():
+            raise serializers.ValidationError(
+                "This stylist already has a booking at this date and time."
+            )
+
+        return data
+
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
