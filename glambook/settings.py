a9ce56fd8d6a1c2f0e1aa5f64a6708d0dece3d86
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-07-23 08:22:57 +0300

    Configure Django settings for deployment

--- glambook/settings.py
+++ glambook/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c#_umkis!-h(ypc@7jn9%@hwvt88-cqw*w64^y_qihqqgdbxy='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
